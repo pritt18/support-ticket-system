d21600782b9ad771a2336d6e91ef656e4c1ae3c2
pritt18 <pritamgangurde18@gmail.com>
2026-02-22 15:35:53 +0530

    Updated ALLOWED_HOSTS for production

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -34,7 +34,11 @@ SECRET_KEY = 'django-insecure-@065tk5p#&8i3=!@n8xb+qm#ua^ce@@h4=sb(4w($@jveo#gqm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "support-ticket-system-3.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition

f1193b3f5367c7b4662ec4ca62bd5c23d580c94a
pritt18 <pritamgangurde18@gmail.com>
2026-02-22 21:32:38 +0530

    Added STATIC_ROOT for production

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -131,5 +131,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_URL = 'static/'
 CORS_ALLOW_ALL_ORIGINS = True
+

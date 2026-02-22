909f5ef8aca01f551a42f949ccdb9015cf53556c
pritt18 <pritamgangurde18@gmail.com>
2026-02-22 15:41:40 +0530

    Added homepage route

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,3 +3,11 @@ from django.urls import path, include
 urlpatterns = [
     path('api/', include('tickets.urls')),
 ]
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("AI Support Ticket System is Live 🚀")
+
+urlpatterns = [
+    path('', home),
+]
